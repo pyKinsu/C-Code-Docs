c2c66804fc4225fb144ce5dd715788d518970ee5
Kinsu Rajput <kissuxrajput@gmail.com>
2025-10-13 11:02:11 +0530

    Create area-rectangle.c

---
+++ examples/area-rectangle.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+int main() {
+    float lenght, breadth, area;
+    
+    printf("Enter Lenght of Rectangle:");
+    scanf("%f", &lenght);
+    
+    printf("Enter Breadth of Rectangle:");
+    scanf("%f", &breadth);
+    
+    area = lenght*breadth;
+    printf("The Total rea Of Rectangle %f", area);
+    
+    
+
+    return 0;
+}
