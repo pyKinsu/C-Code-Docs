f83c03af2b164d3d00baa59fbc8fc1bb82b1ecaf
Kinsu Rajput <kissuxrajput@gmail.com>
2025-11-04 10:15:46 +0530

    Update recharge-plan.c

--- examples/recharge-plan.c
+++ examples/recharge-plan.c
@@ -1,3 +1,14 @@
+/*
+Write a Program in C to implement Mobile Recharge Plan Selector
+Display plan options:
+Rs99-  1GB/day for 14 days
+Rs199 – 1.5GB/day for 28 days
+Rs299 – 2GB/day for 28 days
+Rs 499 – 2.5GB/day for 56 days
+Use switch to show plan details after user selects a number.
+written by @pykinsu
+*/
+
 #include <stdio.h>
 
 int main () {
