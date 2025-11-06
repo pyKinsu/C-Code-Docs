707c6894e6568fbfe6365f68d0723f90560a9df9
Kinsu Rajput <kissuxrajput@gmail.com>
2025-11-06 08:52:14 +0530

    Update fibonacciusingforloop.c

--- examples/fibonacciusingforloop.c
+++ examples/fibonacciusingforloop.c
@@ -1,6 +1,35 @@
 // Write A c Program To Show fibonacci Series
 // written By @Pykinsu
 
+/* This Is V1 And V2 Is Below */
+
+#include <stdio.h>
+
+int main() {
+    int limit, series, num1, num2;
+    num1=0;
+    num2=1;
+    
+    printf("Enter The Series Limit ");
+    scanf("%d",&limit);
+    
+    printf("%d,%d,",num1, num2);
+    
+    for(int i=2; i<=limit; i++)
+    {
+        series=num1+num2;
+        printf("%d,",  series);
+        num1=num2;
+        num2=series;
+    }
+
+    return 0;
+}
+
+
+/* This Is V2 And V1 Is above */
+
+
 #include <stdio.h>
 
 int main() {
