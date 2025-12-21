8f7690e9a14019e830f8f9f9e1b4798f2a1085a0
Kinsu Rajput <kissuxrajput@gmail.com>
2025-12-21 07:53:30 +0530

    Update loginpageusingstring.c

--- examples/loginpageusingstring.c
+++ examples/loginpageusingstring.c
@@ -1,3 +1,42 @@
+
+// Login Using C Maximum Tries 3
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    char name[20];
+    char pwd[20];
+    int count=0;
+    do{
+        printf("\nEnter User name\n");
+        scanf("%s",name);
+        printf("Enter Password\n");
+        scanf("%s",pwd);
+        if(strcmp("admin",name)==0 && strcmp("admin123",pwd)==0)
+        {
+            printf("\nWelcome");
+            exit(0);
+        }
+        else{
+            if(count==2)
+            {
+                printf("\n Maximum 3 Chance allowed only");
+                exit(0);
+            }
+            printf("\nTry again");
+            count=count+1;
+
+
+        }
+    }while(1);
+    return 0;
+}
+
+/*–------------------------------------------–---------------------------------------*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
